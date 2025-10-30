a81a8dc8d7316181debb5bc880f533ebc71c812d
d.kanai <d.kanai@dkanais-MacBook-Pro.local>
2025-10-30 14:24:47 +0900

    Tidy test output and quiet logging

--- backend/src/main/resources/db/migration/V2__Convert_users_id_to_uuid.sql
+++
@@ -1,9 +0,0 @@
-${create_pgcrypto_extension}
-
-ALTER TABLE users ALTER COLUMN id DROP DEFAULT;
-${drop_users_id_sequence}
-
-ALTER TABLE users
-    ALTER COLUMN id ${users_id_uuid_type};
-
-ALTER TABLE users ALTER COLUMN id SET DEFAULT ${users_id_uuid_default};
